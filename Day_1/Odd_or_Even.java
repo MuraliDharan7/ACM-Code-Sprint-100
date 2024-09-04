07ea13edd1ba126edcd5fa203dd4494d9e810e7a
MuraliDharan7 <ngmurali007@gmail.com>
2024-09-04 16:56:42 +0530

    Odd even added

---
+++ Day_1/Odd_or_Even.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Odd_or_Even{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        if(num==0){
+            System.out.println("Number is Zero");
+        }
+        else if(num%2==0){
+            System.out.println("Number is Even");
+        }
+        else{
+            System.out.println("Number is ODD");
+        }
+        sc.close();
+    }
+}
