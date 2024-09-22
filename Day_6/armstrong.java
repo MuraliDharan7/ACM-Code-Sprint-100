701bc50a731ad09af5fb77a9644011398f4c26ab
MuraliDharan7 <ngmurali007@gmail.com>
2024-09-23 00:12:52 +0530

    added Day 6

---
+++ Day_6/armstrong.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static boolean checkArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += (digit * digit * digit); // Correct the summing logic
+            n /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+
+        if (checkArmstrong(n)) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+
+        sc.close();
+    }
+}
+
